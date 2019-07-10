9dd2a9262034c0cf5740c75ddb6671d3f7805892
Umesh Nalinde <Umeshnalindeelite@gmail.com>
2019-07-10 14:10:39 +0530

    fix firestore

--- js/firestore.js
+++ js/firestore.js
@@ -1,11 +1,27 @@
-function saveMessage() {
-    // Add a new message entry to the Firebase database.
-    return firebase.firestore().collection('db').add({
+var firebaseConfig = {
+    apiKey: "AIzaSyBdEffKaDXvwCyEYUUYgi-nYpdY5BRu6mY",
+    authDomain: "personal-website-15cb0.firebaseapp.com",
+    databaseURL: "https://personal-website-15cb0.firebaseio.com",
+    projectId: "personal-website-15cb0",
+    storageBucket: "",
+    messagingSenderId: "78664630668",
+    appId: "1:78664630668:web:f57f1fb53f17a079"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function onClick() {
+
+    firebase.firestore().collection('data').add({
         name: 'Umesh Nalinde',
         email: 'UmeshPande6@gmail.com',
         subject: 'Hello',
         message: 'Message'
+    }).then(function () {
+        window.alert("Submit Successful");
     }).catch(function (error) {
-        console.error('Error writing new message to Firebase Database', error);
+        console.error('Error writing new message to Firebase Database', error).then(function () {
+            window.alert("Error");
+        });
     });
 }
