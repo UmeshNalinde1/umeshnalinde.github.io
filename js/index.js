91d57d91b686f21b7b83be5ee66d70d7632bf20f
Umesh Nalinde <Umeshnalindeelite@gmail.com>
2019-07-10 14:01:31 +0530

    fix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-
-
-var config = {
+var firebaseConfig = {
     apiKey: "AIzaSyBdEffKaDXvwCyEYUUYgi-nYpdY5BRu6mY",
     authDomain: "personal-website-15cb0.firebaseapp.com",
     databaseURL: "https://personal-website-15cb0.firebaseio.com",
@@ -8,11 +6,12 @@ var config = {
     storageBucket: "",
     messagingSenderId: "78664630668",
     appId: "1:78664630668:web:f57f1fb53f17a079"
-}
-firestore.initializeApp(config);
-const docRef = firestore.firestore().doc("db/4h1h9rQb42JiiTYLwgfx");
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 function onClick() {
-    docRef.set({
+
+    firebase.firestore().collection('data').add({
         name: 'Umesh Nalinde',
         email: 'UmeshPande6@gmail.com',
         subject: 'Hello',
